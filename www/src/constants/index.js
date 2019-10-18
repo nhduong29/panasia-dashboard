21b37ff95178ad571770ea6c6419c77337d03feb
nhduong29 <nhduong29@gmail.com>
2019-10-19 01:24:53 +0700

    add lid to read env file

--- www/src/constants/index.js
+++ www/src/constants/index.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv');
+const dotenv = require('./dotenv/main.js');
 dotenv.config();
 
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
