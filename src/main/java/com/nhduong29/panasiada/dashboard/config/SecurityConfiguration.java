0be9e9e1ec94f278ecb69f8c405b61388f51dc44
nhduong29 <nhduong29@gmail.com>
2019-10-19 00:41:51 +0700

    edit port for production

--- src/main/java/com/nhduong29/panasiada/dashboard/config/SecurityConfiguration.java
+++ src/main/java/com/nhduong29/panasiada/dashboard/config/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.nhduong29.panasiada.dashboard.config;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +18,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SimpleSavedRequest;
 
 import com.nhduong29.panasiada.dashboard.config.security.CustomUserDetailsService;
 import com.nhduong29.panasiada.dashboard.config.security.jwt.JwtAuthenticationEntryPoint;
@@ -57,10 +63,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
 				.antMatchers("/", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg", "/**/*.html",
 						"/**/*.css", "/**/*.js")
-				.permitAll().antMatchers("/api/auth/**").permitAll()
-				.antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**").permitAll().anyRequest().authenticated();
+				.permitAll().antMatchers("/api/auth/**").permitAll().antMatchers("/api/user/checkUsernameAvailability")
+				.permitAll().antMatchers(HttpMethod.GET, "/api/car/**", "/api/users/**").permitAll().anyRequest()
+				.authenticated();
 		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
+	}
+
+	@Bean
+	public RequestCache refererRequestCache() {
+		return new HttpSessionRequestCache() {
+			@Override
+			public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+				String referrer = request.getHeader("referer");
+				if (referrer != null) {
+					request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST",
+							new SimpleSavedRequest(referrer));
+				}
+			}
+		};
 	}
 }
--- www/src/constants/index.js
+++ www/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 export const ACCESS_TOKEN = 'panAsiaAccessToken';
 
 export const CAR_LIST_SIZE = 10;
